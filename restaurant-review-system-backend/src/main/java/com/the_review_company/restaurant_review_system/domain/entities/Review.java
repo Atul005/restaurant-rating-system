4a9d5e78a68a40009a02082f8748a741cff1ac1e
atulyadav005 <yadavatul005@gmail.com>
2025-12-26 20:03:23 +0530

    initial commit, project setup, entities created and frontend setup

---
+++ restaurant-review-system-backend/src/main/java/com/the_review_company/restaurant_review_system/domain/entities/Review.java
@@ -0,0 +1,46 @@
+package com.the_review_company.restaurant_review_system.domain.entities;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(indexName = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Review {
+
+    @Field(type = FieldType.Keyword)
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String content;
+
+    @Field(type = FieldType.Integer)
+    private Integer rating;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    private LocalDateTime datePosted;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    private LocalDateTime lastEdited;
+
+    @Field(type = FieldType.Nested)
+    private List<Photo> photos = new ArrayList<>();
+
+    @Field(type = FieldType.Nested)
+    private User writtenBy;
+
+    @Field(type = FieldType.Nested)
+    private Restaurant restaurant;
+
+}
